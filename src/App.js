a6e958305bbbf791a64cfcd09ad4cc5647160d77
Nimrod Shefi <shefin@cardiff.ac.uk>
2021-11-07 17:55:36 +0000

    created the login page for the app, but beyond sending the response to the console, the google login doesn't do anything yet

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import GoogleLogin from 'react-google-login';
 
-function App() {
-  return (
-    <body>
-      <div id="app_details">
-        <h1>Application Name</h1>
-        <p>By logging in to your Google Account here, you are able to provide yourself increased privacy from Google's alogirthm by letting us visit many website under your name. 
-          To ensure that you are completely comfortable in using our services, you are able to opt out of us loading some of the webistes on the list if you don't want. 
-          In addition to that, to ensure your privacy, the moment you close the app, all the data stored about you in our systems will be deleted, unless you decide otherwise.</p>
+export class App extends Component {
+
+  responseGoogle=(response)=>{
+    console.log(response);
+    console.log(response.profileObj);
+  }
+
+  render() {
+    return (
+      <body>
+        <div id="app_details">
+          <h1>Application Name</h1>
+          <p>By logging in to your Google Account here, you are able to provide yourself increased privacy from Google's alogirthm by letting us visit many website under your name. 
+            To ensure that you are completely comfortable in using our services, you are able to opt out of us loading some of the webistes on the list if you don't want.
+            <br/><br />
+            In addition to that, to ensure your privacy, the moment you close the app, all the data stored about you in our systems will be deleted, unless you decide otherwise.</p>
+        </div>
+
+        <div id="app_login">
+        <GoogleLogin
+        clientId="919197055743-dfarr65584u8g1nsogshtohr9l2e1m3i.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={this.responseGoogle}
+        onFailure={this.responseGoogle}
+        cookiePolicy={'single_host_origin'}
+        />
       </div>
-      <script src="https://apis.google.com/js/platform.js" async defer></script>
-    </body>
-  );
+      </body>
+
+    )
+  }
 }
 
 export default App;
+
+
+
