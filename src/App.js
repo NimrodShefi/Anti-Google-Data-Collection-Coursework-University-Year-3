fc7468e23b6cf0d95bf0ac58cea634715c8db48e
Nimrod Shefi <shefin@cardiff.ac.uk>
2021-11-09 10:19:13 +0000

    seperating the login component from the App.js file and not will try and also add routes to the app

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
 import './App.css';
-import React, {Component} from 'react';
-import GoogleLogin from 'react-google-login';
+import React from "react";
+import Login from './components/Login';
 
-export class App extends Component {
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  )
 
-  responseGoogle=(response)=>{
-    console.log(response);
-    console.log(response.profileObj);
-  }
-
-  render() {
-    return (
-      <body>
-        <div id="app_details">
-          <h1>Application Name</h1>
-          <p>By logging in to your Google Account here, you are able to provide yourself increased privacy from Google's alogirthm by letting us visit many website under your name. 
-            To ensure that you are completely comfortable in using our services, you are able to opt out of us loading some of the webistes on the list if you don't want.
-            <br/><br />
-            In addition to that, to ensure your privacy, the moment you close the app, all the data stored about you in our systems will be deleted, unless you decide otherwise.</p>
-        </div>
-
-        <div id="app_login">
-        <GoogleLogin
-        clientId="919197055743-dfarr65584u8g1nsogshtohr9l2e1m3i.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={this.responseGoogle}
-        onFailure={this.responseGoogle}
-        cookiePolicy={'single_host_origin'}
-        />
-      </div>
-      </body>
-
-    )
-  }
 }
 
 export default App;
 
-// Used this youtube tutorial to help me create the google login:
-// https://www.youtube.com/watch?v=-OgU5EAcQmo
+// Used this youtube tutorial to help me create the multiple page react:
+// https://www.youtube.com/watch?v=xMNhDf5-hvk&ab_channel=ConorBailey
---
+++ src/components/Login.js
@@ -0,0 +1,40 @@
+import './Login.css';
+import React, { Component } from "react";
+import GoogleLogin from "react-google-login";
+
+export class Login extends Component {
+
+    responseGoogle = (response) => {
+        console.log(response);
+        console.log(response.profileObj);
+    }
+
+    render() {
+        return (
+            <div>
+                <div id="app_details">
+                    <h1>Application Name</h1>
+                    <p>By logging in to your Google Account here, you are able to provide yourself increased privacy from Google's alogirthm by letting us visit many website under your name.
+                        To ensure that you are completely comfortable in using our services, you are able to opt out of us loading some of the webistes on the list if you don't want.
+                        <br /><br />
+                        In addition to that, to ensure your privacy, the moment you close the app, all the data stored about you in our systems will be deleted, unless you decide otherwise.</p>
+                </div>
+
+                <div id="app_login">
+                    <GoogleLogin
+                        clientId="919197055743-cr391ut1ptdgkaj5e06tb8icgi1477di.apps.googleusercontent.com"
+                        buttonText="Login"
+                        onSuccess={this.responseGoogle}
+                        onFailure={this.responseGoogle}
+                        cookiePolicy={'single_host_origin'}
+                    />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Login;
+
+// Used this youtube tutorial to help me create the google login:
+// https://www.youtube.com/watch?v=-OgU5EAcQmo
