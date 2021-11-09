edacb0669b616465f4d141b0ae9aa575a84ffd4f
Nimrod Shefi <shefin@cardiff.ac.uk>
2021-11-09 11:23:52 +0000

    making the component load according to the url path. Need to find how to do the background services with this now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from "react";
 import Login from './components/Login';
+import { Route, Routes } from 'react-router';
 
 function App() {
   return (
     <div>
-      <Login />
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
     </div>
   )
 
@@ -13,5 +16,7 @@ function App() {
 
 export default App;
 
-// Used this youtube tutorial to help me create the multiple page react:
+// Used this youtube tutorial to help me create the multiple page:
 // https://www.youtube.com/watch?v=xMNhDf5-hvk&ab_channel=ConorBailey
+// to help me finish, as he was showing version 5, when I was using version 6:
+// https://stackoverflow.com/questions/69832748/error-a-route-is-only-ever-to-be-used-as-the-child-of-routes-element
